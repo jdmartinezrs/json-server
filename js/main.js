68ea2e81051333881618b24bc8943eb804ca9699
CampusLands <139265002+CampusLands@users.noreply.github.com>
2024-04-22 14:38:06 -0500

    feat: :card_file_box: Datos a quemar

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import prompt from 'async-prompt'
-import { getAllProfile, postProfile } from './module/camper.js'
-
-// console.log(await getAllProfile());
-
-let name = await prompt("Ingrese el nombre: ");
-let edad = await prompt("Ingrese la edad: ");
-console.log(await postProfile(name, edad));
