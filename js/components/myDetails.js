ed30862674278a9432b44cce8f17be8c363da6fb
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-05-08 17:53:46 -0500

    feat: :sparkles: Implementing web components that  shows querys

---
+++ js/components/myDetails.js
@@ -0,0 +1,19 @@
+export class Mydetails extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode:"open"})
+        this.shadowRoot.innerHTML = /*html*/`
+        <link rel="stylesheet" href="../css/myDetails.css">
+            <details id="queryAboutTable7">
+            <summary>
+                <div class="details__description">Campus: </div>
+                <div class="details__container">
+                    <p><marquee behavior="" direction="">Consultas sobre una tabla 6.Devuelve un listado con el nombre de los todos los clientes españoles.</marquee></p>
+                </div>
+            </summary>
+            <div class="report__container"></div>
+            </details> 
+        `
+    }
+}
+ 
